25d1792ea78599d232f2def714d69f3dc1fc9dcc
vmukund <v_mukund@yahoo.com>
2024-07-14 19:18:46 +0530

    Create providers.tf -Initial commit
    Providers

---
+++ kibanadeployment/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+ 
