6e7c174b7b3e51c9d436e2331b6e9a78e4047452
vmukund <v_mukund@yahoo.com>
2024-07-21 21:40:12 +0530

    Create main.tf - initial commit

---
+++ deployopentelemetryop/main.tf
@@ -0,0 +1,43 @@
+provider "kubernetes" {
+  config_context = var.kubectl_config_context_name
+  config_path    = var.kubectl_config_path
+}
+
+provider "helm" {
+  kubernetes {
+   config_context = var.kubectl_config_context_name
+   config_path    = var.kubectl_config_path
+  }
+}
+
+resource "kubernetes_namespace" "otel-namespace" {
+#  count      = var.create_resources ? 1 : 0
+  metadata {
+    name        = var.name
+    labels      = var.labels
+    annotations = var.annotations
+  }
+}
+
+resource "helm_release" "opentelemetry-operator" {
+  name                = "opentelemetry-operator"
+#  namespace           = "otel-operator"
+#  create_namespace    = true
+  repository          = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart               = "opentelemetry-operator"
+  version             = "0.48.0"
+  namespace           = kubernetes_namespace.otel-namespace.metadata.0.name
+  
+
+#  values = [
+#    file("${path.module}/values.yaml")
+#  ]
+
+#  set {
+#    name  = "manager.collectorImage.repository"
+#    value = ""
+#  }
+
+   depends_on = [kubernetes_namespace.otel-namespace]
+}
+
