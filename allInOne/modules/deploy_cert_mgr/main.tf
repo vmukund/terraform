c49e6498bcc8e34d161146211a162500ebbbfd57
vmukund <v_mukund@yahoo.com>
2024-07-28 21:03:07 +0530

    Create main.tf - Initial Commit

---
+++ allInOne/modules/deploy_cert_mgr/main.tf
@@ -0,0 +1,40 @@
+
+
+# Create cert-manager namespace
+resource "kubernetes_namespace" "cert_manager_namespace" {
+  metadata {
+    name = var.namespace
+  }
+}
+
+# Install cert-manager helm chart
+resource "helm_release" "cert_manager" {
+  name             = var.helm_release_name
+  repository       = var.helm_repository_url
+  chart            = var.helm_chart_name
+# namespace        = var.namespace
+  replace          = "true"
+  reuse_values      = true
+  force_update      = true
+  wait              = true
+  max_history       = 1
+  version           = "1.14.5"
+  namespace         = kubernetes_namespace.cert_manager_namespace.metadata[0].name
+# skip_crds         = true
+  dependency_update = true
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+ 
+  values = [
+    templatefile("${path.module}/values.yaml.tpl", {
+      replica_count = var.replica_count
+    })
+  ]
+
+  depends_on = [ 
+    kubernetes_namespace.cert_manager_namespace
+   ]
+}
